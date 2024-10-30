576cacb6ef6bdb5d65e7f7b0e81b88b62afaae28
[BOT]-Rhythm's-File-Uploder <rhythm.upa@gmail.com>
2024-10-30 19:31:10 +0530

    [BOT] Updation TO my Webste

--- SiteMap/script.js
+++ SiteMap/script.js
@@ -3,11 +3,10 @@ function link(n) {
         'https://ms7ru.github.io/School-Tools',
         'https://ms7ru.github.io/NotePad',
         'https://ms7ru.github.io/clock',
-        'https://ms7ru.github.io/maze/',
         'https://ms7ru.github.io/t',
         'https://ms7ru.github.io/OHIO',
-        'https://ms7ru.github.io/PcPartPicker/',
-        'https://drive.google.com/drive/folders/1ZltQTmU7Db10QA9L2z3AP_8waFz54_vr'
+        'https://ms7ru.github.io/NewTab',
+        'https://ms7ru.github.io/WIPs-(work-in-progress)'
     ];
     window.location.href = links[n-1]
 }
