51e5c43a3694dfec9dafd2212f2cec81eb9d92d5
Rhythm <88928636+ms7ru@users.noreply.github.com>
2024-11-30 15:42:41 +0530

    Update script.js

--- clock/script.js
+++ clock/script.js
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>World Time</title>
-</head>
-<body>
-    <div>
-        <p id="date"></p>
-        <p id="clock"></p>
-    </div>
-    <script>
-        let lastTime = null;
+let lastTime = null;
 
         // Update the date
         function updateDate() {
@@ -67,6 +54,3 @@
 
         // Initialize the clock on page load
         window.onload = updateTime;
-    </script>
-</body>
-</html>
